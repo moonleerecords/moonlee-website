afe81f1b870c3ed96e23e171b670f76ba6d024d5
Drazen Peric <peric.drazhen@gmail.com>
2016-04-26 20:54:44 +0200

    Added some basic translations

--- app/models/release_type.rb
+++ app/models/release_type.rb
@@ -5,13 +5,6 @@ class ReleaseType < ApplicationRecord
   validates :format, presence: true, inclusion: { in: :allowed_formats }
 
   def allowed_formats
-    # TODO: translations
-    # cd - CD
-    # lp - LP
-    # 7inch - 7"
-    # download - Download
-    # lp_cd - LP + CD
-    # download_and_merch - Download + Merch
     %w(cd lp 7inch download lp_cd download_merch)
   end
 end
