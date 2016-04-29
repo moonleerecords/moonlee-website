05aafe8ff605a13667bce19cfd2d9ba5eab3c8db
Drazen Peric <peric.drazhen@gmail.com>
2016-04-29 09:43:56 +0200

    Updated deploy configs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,5 @@
-server '46.101.101.140', user: 'deploy', port: 22, roles: [:web, :app, :db], primary: true
+set :user, 'deploy'
+set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :ssh_options, forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub)
+
+server '46.101.101.140', port: 22, roles: [:web, :app, :db], primary: true
