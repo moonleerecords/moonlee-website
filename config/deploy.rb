638be7d58ea3d78ad40de61abc7ce6806088bcdc
Drazen Peric <peric.drazhen@gmail.com>
2016-05-01 13:51:20 +0200

    Updated gems

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :puma_init_active_record, true # Change to false when not using ActiveRecord
 
 ## Linked Files & Directories (Default None):
 set :linked_files, %w(config/application.yml)
-set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs,  %w(bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system)
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
