ea4319be41b25268a33f1e41525f95e89dd26a59
Drazen Peric <peric.drazhen@gmail.com>
2016-05-01 14:26:52 +0200

    Changed deployment configs (puma finally works)

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,11 +8,6 @@ set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true # Change to false when not using ActiveRecord
