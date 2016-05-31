34896c2aa175b1881b9b5763a7f1663e3a2135c0
Drazen Peric <peric.drazhen@gmail.com>
2016-05-31 23:06:33 +0200

    Removed trailing whitespace

--- app/controllers/records/home_controller.rb
+++ app/controllers/records/home_controller.rb
@@ -1,6 +1,6 @@
 class Records::HomeController < Records::BaseController
   # TODO: before_filter :search_deal, :only => [:showcase, :buy_stuff, ...]
-  # TODO: upcoming releases, shown on more pages?
+  # TODO: upcoming releases, shown on more pages, but different count
 
   def index
     # TODO: @latest_news = News.all
@@ -11,6 +11,5 @@ class Records::HomeController < Records::BaseController
 
     @upcoming_events = upcoming_events_by_artist(5)
     # TODO: latest adds to the store
-    
   end
 end
