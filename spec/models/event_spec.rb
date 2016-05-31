142353a55d96b9491bf0510f3e9ff9e7ec6f5710
Drazen Peric <peric.drazhen@gmail.com>
2016-05-31 23:05:47 +0200

    Fixed test

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Event, type: :model do
   before(:context) do
+    @artist = Artist.new
+    @artist.name = 'Repetitor'
+    @artist.save
+
     @venue = Venue.new
     @venue.name = 'Klub Gromka'
     @venue.city = 'Ljubljana'
@@ -14,6 +18,7 @@ RSpec.describe Event, type: :model do
   context 'on saving' do
     it 'should save Event if all validations pass' do
       event = Event.new
+      event.artist = @artist
       event.venue = @venue
       event.start_date = Time.zone.today + 10.days
 
