4fc0ce8f2ac312fb027732498a5b220f1611a18f
Drazen Peric <peric.drazhen@gmail.com>
2016-06-03 15:21:35 +0200

    Fixed test

--- spec/controllers/records/releases_controller_spec.rb
+++ spec/controllers/records/releases_controller_spec.rb
@@ -9,8 +9,16 @@ RSpec.describe Records::ReleasesController, type: :controller do
   end
 
   describe 'GET #show' do
+    before do
+      release = Release.new
+      release.title = 'Dobrodošli na okean'
+      release.catalog_number = 'HMRL028'
+      release.release_date = Date.new(2012, 11, 29)
+      release.save
+    end
+
     it 'returns http success' do
-      get :show, params: { id: 1 }
+      get :show, params: { id: 'dobrodosli-na-okean' }
       expect(response).to have_http_status(:success)
     end
   end
