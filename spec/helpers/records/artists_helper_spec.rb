a545b11930b5f5727b518e90030430b35fa01256
Drazen Peric <peric.drazhen@gmail.com>
2016-06-05 21:42:44 +0200

    Fixed test

--- spec/helpers/records/artists_helper_spec.rb
+++ spec/helpers/records/artists_helper_spec.rb
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe Records::ArtistsHelper, type: :helper do
+RSpec.describe ArtistsHelper, type: :helper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
