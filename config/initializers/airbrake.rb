ce1d32aeb348f5ebf3ea403b4a77b77211ba09a2
Drazen Peric <peric.drazhen@gmail.com>
2016-09-18 13:42:47 +0200

    Ignored some rubocop checks

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -13,7 +13,7 @@ Airbrake.configure do |c|
   # from the right sidebar.
   # https://github.com/airbrake/airbrake-ruby#project_id--project_key
   c.project_id = 128171
-  c.project_key = ENV["AIRBRAKE_PROJECT_KEY"]
+  c.project_key = ENV['AIRBRAKE_PROJECT_KEY']
 
   # Configures the root directory of your project. Expects a String or a
   # Pathname, which represents the path to your project. Providing this option
