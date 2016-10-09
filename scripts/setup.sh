8d6e5fbab67af072662a8d85f501f24135c583af
Drazen Peric <peric.drazhen@gmail.com>
2016-10-09 18:19:22 +0200

    Set default env

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Setup local environment - db recreate, fetch events
-rails db:drop db:create db:migrate db:seed db:songkick:fetch_upcoming_events
+rails RAILS_ENV=development db:drop db:create db:migrate db:seed db:songkick:fetch_upcoming_events
