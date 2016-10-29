628587d9cd32fba7a29dbe039adc8273a926bfbd
Drazen Peric <peric.drazhen@gmail.com>
2016-10-29 21:09:48 +0200

    Done with social boxes

--- db/migrate/20161008171548_create_social_posts.rb
+++ db/migrate/20161008171548_create_social_posts.rb
@@ -2,10 +2,11 @@ class CreateSocialPosts < ActiveRecord::Migration[5.0]
   def change
     create_table :social_posts do |t|
       t.string :source
-      t.string :link
+      t.string :url
       t.string :external_id
       t.text :text
       t.text :media
+      t.text :video_media
       t.datetime :published_at
       t.boolean :active, default: true
 
