cfd7952f80beb2ccdfcdb088820978d0f99d237d
Drazen Peric <peric.drazhen@gmail.com>
2016-11-01 19:48:14 +0100

    Fixed test

--- spec/models/social_post_spec.rb
+++ spec/models/social_post_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe SocialPost, type: :model do
       social_post.source = SocialPost::SOURCE_INSTAGRAM
       social_post.url = 'https://instagram.flju1-1.fna.fbcdn.net/t51.2885-15/s320x320/e35/14659235_183405935400735_5440615191886692352_n.jpg?ig_cache_key=MTM1NzI3MzA5OTI2NzE5NjMzOA%3D%3D.2'
       social_post.external_id = '1357273099267196338_241028993'
+      social_post.published_at = Time.now
 
       expect(social_post.valid?).to eq(true)
     end
