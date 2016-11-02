5abf7a5d8e64f3e4d49245a0e98c8aa6624eb03f
Drazen Peric <peric.drazhen@gmail.com>
2016-11-02 22:16:39 +0100

    Added active state to navigation links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def navigation_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'active' : ''
+
+    content_tag(:li) do
+      link_to link_text, link_path, class: class_name
+    end
+  end
 end
