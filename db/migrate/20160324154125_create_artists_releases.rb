272a542fe789df8115b6b855fce258c1086fb4f7
Drazen Peric <peric.drazhen@gmail.com>
2016-11-27 22:38:44 +0100

    Updated migration file

--- db/migrate/20160324154125_create_artists_releases.rb
+++ db/migrate/20160324154125_create_artists_releases.rb
@@ -4,5 +4,6 @@ class CreateArtistsReleases < ActiveRecord::Migration[5.0]
       t.belongs_to :artist, index: true
       t.belongs_to :release, index: true
     end
+    add_index :artists_releases, [:artist_id, :release_id], unique: true
   end
 end
