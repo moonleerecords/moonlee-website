dc1abb49ac7036d480a42f3090cf7173926f8481
Drazen Peric <peric.drazhen@gmail.com>
2016-12-17 18:52:03 +0100

    Changed schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,5 @@
 set :output, '/var/log/cron_log.log'
 
-every 12.hours do
-  runner 'Fetch upcoming events from Songkick'
-  rake 'songkick:fetch_upcoming_events'
-end
-
 every 6.hours do
   runner 'Fetch images from Instagram'
   rake 'social:fetch_instagram'
@@ -18,4 +13,9 @@ end
 every 6.hours, at: 20 do
   runner 'Fetch posts from Twitter'
   rake 'social:fetch_twitter'
+end
+
+every 6.hours, at: 30 do
+  runner 'Fetch upcoming events from Songkick'
+  rake 'songkick:fetch_upcoming_events'
 end
