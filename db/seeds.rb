6ac1a1f771f6faa5c85044f54cb06677d6e5479a
Drazen Peric <peric.drazhen@gmail.com>
2017-01-04 23:11:35 +0100

    Changed image extensions

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,21 +20,21 @@ importer.posts
 # Slides
 Slide.create(
   title: 'Repetitor released new album "Gde Ćeš"',
-  image: File.new(Rails.root.join('lib', 'seeds', 'images', 'slides', 'repetitor_001.png')),
+  image: File.new(Rails.root.join('lib', 'seeds', 'images', 'slides', 'repetitor_001.jpg')),
   url: '/artists/repetitor',
   records: true,
   position: 1
 )
 Slide.create(
   title: 'Debeli Precjednik / Mašinko split release out now!',
-  image: File.new(Rails.root.join('lib', 'seeds', 'images', 'slides', 'debeli_precjednik_001.png')),
+  image: File.new(Rails.root.join('lib', 'seeds', 'images', 'slides', 'debeli_precjednik_001.jpg')),
   url: '/artists/debeli-precjednik-fat-prezident',
   records: true,
   position: 2
 )
 Slide.create(
   title: 'Bernays Propaganda with new album "Politika"',
-  image: File.new(Rails.root.join('lib', 'seeds', 'images', 'slides', 'bernays_propaganda_001.png')),
+  image: File.new(Rails.root.join('lib', 'seeds', 'images', 'slides', 'bernays_propaganda_001.jpg')),
   url: '/artists/bernays-propaganda',
   records: true,
   position: 3
